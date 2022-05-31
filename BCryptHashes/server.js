190ab690cd8a86a8c31940588faac9a8cc12fcc8
markn <markn@aesclever.com>
2022-05-31 15:44:07 -0700

    Hash password using Bcrypt Sync and Async

--- BCryptHashes/server.js
+++ BCryptHashes/server.js
@@ -7,47 +7,39 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
+const bcrypt = require('bcrypt');
+
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+bcrypt
+  .hash(myPlaintextPassword, saltRounds)
+  .then((hash) => {
+    return bcrypt.compare(myPlaintextPassword, hash)
+    .then((result) => {
+      console.log("myPlaintextPassword:", result); // generic: true
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 //END_ASYNC
 
 //START_SYNC
+bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+  console.log(hash);
+  // The hash returned, continue to compare
+  bcrypt.compare(myPlaintextPassword, hash, function (err, result) {
+    console.log("generic:", result); // generic: true
+  });
 
-
-
+  bcrypt.compare(someOtherPlaintextPassword, hash, function (err, result) {
+    console.log("someOtherPlaintextPassword:", result); // falsy: false
+  });
+});
 //END_SYNC
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(process.env.PORT || 3000, () => {});
+
